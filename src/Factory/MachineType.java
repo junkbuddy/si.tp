2fe432cbb7cef769c59db6ca8b2f61b2932d7feb
JLuisCosta <JLuisCosta@172.20.100.141>
2013-05-24 15:46:02 +0100

    Enumeração para identificar os tipos de máquinas existentes na linha de produção.

---
+++ src/Factory/MachineType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+/**
+ * 
+ * @author Group 7
+ */
+public enum MachineType {
+    
+    CUTTING,PACKING,SEWING
+    
+}
