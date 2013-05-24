54820e1431fbbe9aad3111b087e96311434afe33
JLuisCosta <JLuisCosta@172.20.100.141>
2013-05-24 15:59:25 +0100

    Classe abstrata de Máquina.

---
+++ src/Factory/Machine.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+import jade.core.Agent;
+
+/**
+ *
+ * 
+ * Machine.java is parent class of all machine instances and it 
+ * will also contain the common logic applicable in machine making of all types.
+ * 
+ * 
+ * @author Group 7
+ */
+public abstract class Machine extends Agent {
+    
+    private MachineType model = null;
+
+    public Machine(MachineType model) {
+      this.model = model;
+    }
+    
+    protected abstract void construct();
+    
+    public MachineType getModel(){
+        return this.model;
+    }
+    
+    public void setModel(MachineType model){
+        this.model = model;
+    }
+    
+    
+    
+}
